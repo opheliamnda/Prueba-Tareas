cab47fe28d3ce4ca09e3a0aba9d9eda516f00f91
opheliamnda <amanda.leyton@uc.cl>
2026-07-29 11:59:38 -0400

    Update code.py

--- Codigo/code.py
+++ Codigo/code.py
@@ -1,1 +1,78 @@
-# Rellenar con el código solución
+n = int(input())
+ter = 0
+peg = 0
+uni = 0
+
+for _ in range(n):
+  tipo = input()
+  if tipo == "Terrestre":
+    ter += 1
+  elif tipo == "Unicornio":
+    uni += 1
+  elif tipo == "Pegaso":
+    peg +=1
+
+arboles = int(input())
+esta_nublado = input()
+
+caminan = 1 + ter + uni
+
+if esta_nublado == True:
+  caminan += peg
+
+if caminan % 2 == 0:
+  arboles_necesarios = int(caminan/2)
+else:
+  arboles_necesarios = int(caminan/2) +1
+
+
+if arboles_necesarios % 2 == 0:
+  ter_necesarios = int(arboles_necesarios/2)
+else:
+  ter_necesarios = int(arboles_necesarios/2) + 1
+
+
+if arboles_necesarios > arboles:
+  print("No hay suficientes árboles en el entorno para construir el puente")
+else:
+  if uni < arboles_necesarios:
+    print("¡Debemos llamar a más unicornios!")
+  else:
+    if ter_necesarios > ter:
+      print("¡Debemos llamar a más terrestres!")
+    else:
+      print(f"Los unicornios han traído {arboles_necesarios} árboles desde el otro lado para poder cruzar el río")
+      print(f"Se han necesitado {arboles_necesarios} ponys unicornios para traer {arboles_necesarios} árboles")
+      print(f"Los ponys terrestres han talado {arboles_necesarios} árboles para poder cruzar al otro lado del río, obteniendo pasarelas temporales")
+      print(f"Se han necesitado {ter_necesarios} ponys terrestres para talar {arboles_necesarios} árboles")
+      print(f"Han cruzado {ter} ponys terrestres")
+      print(f"Han cruzado {uni} ponys unicornios")
+      print("He cruzado el río")
+      if esta_nublado:
+        print(f"Han cruzado {peg} ponys pegasos caminando")
+      else:
+        print(f"Han cruzado {peg} ponys pegasos volando")
+      print("Hemos cruzado gracias al poder de la amistad")
+
+
+
+"""
+⠀⠀⢀⠤⣂⣤⣬⣭⣭⣭⣔⡠⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠔⣵⣾⣿⣿⣿⢿⣿⣿⣿⣿⣎⢂⠀⢲⣤⣤⣤⣤⣀⣒⣒⣒⣒⣂⡠⠤⠤⣄
+⠐⣾⣿⣿⣿⡏⣾⡿⢎⣛⣫⣭⣴⣾⠆⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢼
+⡇⣿⣿⣿⣿⣟⡿⢀⣐⣻⣛⡩⢁⠀⠀⣘⣛⣛⡛⠿⠿⠿⢿⣿⣿⣿⣿⣿⢟⣾
+⡇⣿⣿⣿⣿⣷⣾⣿⣿⣿⣿⣿⣶⡕⠄⠉⠛⠛⠛⠛⡻⣣⣾⣿⣿⣿⢟⣵⣿⠛
+⠃⣿⣿⣿⣿⣿⢋⣥⠭⡻⣿⣿⣿⣿⡌⡄⠀⠀⠀⡐⣼⣿⣿⣿⡿⣣⣾⠏⠀⠀
+⠨⢻⣿⣿⣿⣧⢻⠁⠀⠘⢸⣿⣿⣿⡇⣿⠀⠀⠌⣼⣿⣿⣿⡿⢱⣿⠃⠀⠀⠀
+⠀⢦⢻⣿⣿⣿⣦⣐⣀⣊⣼⣿⣿⡿⢱⡿⠀⠰⣸⣿⣿⣿⣿⢣⣿⠃⠀⠀⠀⠀
+⠀⠀⠣⣙⠿⣿⣿⣿⣿⣿⣿⠿⢛⣵⡿⠃⢀⢃⣿⣿⣿⣿⡟⣾⡇⠀⠀⠀⠀⠀
+⠀⠀⠀⠈⠛⠶⣮⣭⣭⣴⣶⡿⠿⠋⠀⠀⢨⣘⣿⡻⠿⠿⢇⣿⠀⠀⠀⠀⠀⠀
+⠀⠀⢀⠔⠒⠂⠠⠤⠭⡀⠀⠀⠀⠀⠀⠀⠀⠙⠛⠛⠛⠛⠻⠃⠀⠀⠀⠀⠀⠀
+⢀⠆⠁⠀⡄⠀⠀⠀⠀⠈⢂⠀⠀⠀⠀⠀⠀⠀⠀⢀⡤⠒⠁⠀⠀⠒⢤⡀⠀⠀
+⠣⠤⢤⠞⠂⠀⣀⠰⠃⠀⠘⣆⢀⣀⠀⠀⠀⠀⢀⠎⠀⢠⡀⠀⠀⠀⢀⠀⠙⡀
+⠀⠀⢸⠀⠈⠭⡀⢈⣡⠔⢶⠁⣹⢩⠃⠀⢀⠀⢸⠀⠀⠀⣑⣠⣤⠀⠙⡦⣀⠜
+⠀⠀⠀⠣⠀⢂⠞⠱⠴⣈⡸⠰⢇⠘⠀⠰⡭⠷⢝⡤⣂⣄⠒⢤⡐⠀⠀⡇⠀⠀
+⠀⠀⠀⠀⠱⠄⣀⢜⢁⡠⠥⠊⠀⠀⠀⠀⠡⡘⡄⠐⡂⠘⢌⡀⠉⠂⡸⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠄⠹⢅⣀⠹⠒⠊⠀⠀⠀
+"""
+      
